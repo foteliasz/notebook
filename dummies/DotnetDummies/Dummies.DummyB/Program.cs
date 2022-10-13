d86970b23fd1a644428ccb4f55373dff92a1091c
Wojciech Tomaszek <wojciech.tomaszek@hitachienergy.com>
2022-10-13 16:11:29 +0200

    work in progress 10/13/2022-b

--- dummies/DotnetDummies/Dummies.DummyB/Program.cs
+++ dummies/DotnetDummies/Dummies.DummyB/Program.cs
@@ -1,21 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 
+var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 
-namespace Dummies.DummyB
+var summaries = new[]
 {
-    public static class Program
-    {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-        public static void Main(string[] args)
-        {
-            
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
 
-            app.MapGet("/", () => "Hello World from app B!");
+app.MapGet("/", () => "Hello World from app B!");
 
-            app.Run();
-        }
-    }
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
